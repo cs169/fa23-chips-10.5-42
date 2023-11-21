fbaa21226615d8ff0ce5e1614173d782811c90d0
Jacob Kirschner <jacobk@berkeley.edu>
2023-11-21 14:59:02 +0000

    controller fix

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,4 +4,14 @@ class RepresentativesController < ApplicationController
   def index
     @representatives = Representative.all
   end
+
+  def show
+    @representative = Representative.find(params[:id])
+    @name = @representative.name
+    @ocdid = @representative.ocdid
+    @title = @representative.title
+    @address = @representative.address 
+    @party = @representative.party
+    render 'representatives/show'
+  end
 end
