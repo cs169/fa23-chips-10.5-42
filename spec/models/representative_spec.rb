b176ce0993bc97bb87e77a7140efd8eb0f122340
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2023-11-21 05:54:49 +0000

    Fix linter errors

--- spec/models/representative_spec.rb
+++ spec/models/representative_spec.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Representative do
   describe 'when a representative already exists in the database' do
-    let(:existing_rep) { 
-      Representative.create!(name: 'Rohan Taneja', ocdid: 'ocd-id', title: 'President') 
-    }
+    let(:existing_rep) do
+      described_class.create!(name: 'Rohan Taneja', ocdid: 'ocd-id', title: 'President')
+    end
 
     let(:rep_info) do
       OpenStruct.new(
         officials: [OpenStruct.new(name: 'Rohan Taneja')],
-        offices: [OpenStruct.new(name: 'President', division_id: 'ocd-id', official_indices: [0])]
+        offices:   [OpenStruct.new(name: 'President', division_id: 'ocd-id', official_indices: [0])]
       )
     end
 
     it 'does not create a duplicate one' do
       existing_rep
-      expect { Representative.civic_api_to_representative_params(rep_info) }.not_to change(Representative, :count)
+      expect { described_class.civic_api_to_representative_params(rep_info) }.not_to change(described_class, :count)
     end
   end
 end
