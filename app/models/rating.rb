c896640cc51bff69601cacd8f0c00ea5b8c1dad5
Jacob Kirschner <jacobk@berkeley.edu>
2023-11-29 22:41:31 +0000

    rmed ratings files

--- app/models/rating.rb
+++
@@ -1,8 +0,0 @@
-class Rating < ApplicationRecord
-  belongs_to :news_items
-  belongs_to :user
-
-  def county_names_by_id
-    county&.state&.counties&.map { |c| [c.name, c.id] }.to_h || []
-  end
-end
--- db/migrate/20231128160345_fix_belongs_to_users_in_ratings.rb
+++
@@ -1,8 +0,0 @@
-class FixBelongsToUsersInRatings < ActiveRecord::Migration[5.2]
-  def change
-    change_table :ratings do |t|
-      t.remove_references :user
-      t.belongs_to :users
-    end
-  end
-end
